33e9bfc8699057a78c2670685f7ba1e4b019ba0f
farnaz khoshgoftar <farnaz.khoshgoftar@gmail.com>
2021-08-22 21:34:13 +0430

    Change a word

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,7 +5,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-        <p>This site is the bitpit code challenge </p>
+        <p>This site is the bitpin code challenge </p>
     </footer>
   );
 };
